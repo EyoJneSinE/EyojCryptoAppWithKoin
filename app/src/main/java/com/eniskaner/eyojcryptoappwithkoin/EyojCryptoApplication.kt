47abc7269db2e75b048068e00b1841e383eca822
Enis <enis.d.kaner@gmail.com>
2023-07-16 15:25:06 +0300

    EyojCryptoApplication added for Hilt

---
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/EyojCryptoApplication.kt
@@ -0,0 +1,8 @@
+package com.eniskaner.eyojcryptoappwithkoin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EyojCryptoApplication: Application() {
+}
