034417e02a1628599d5f6af23a13a298aab7d5e5
Enis <enis.d.kaner@gmail.com>
2023-07-16 15:20:22 +0300

    CryptoRepository added

---
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/repo/CryptoRepository.kt
@@ -0,0 +1,31 @@
+package com.eniskaner.eyojcryptoappwithkoin.repo
+
+import com.eniskaner.eyojcryptoappwithkoin.model.Crypto
+import com.eniskaner.eyojcryptoappwithkoin.model.CryptoAllList
+import com.eniskaner.eyojcryptoappwithkoin.service.CryptoAPI
+import com.eniskaner.eyojcryptoappwithkoin.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class CryptoRepository @Inject constructor(
+    private val api: CryptoAPI
+) {
+    suspend fun getCyrptoList() : Resource<CryptoAllList> {
+        val response = try {
+            api.getCyrptoList()
+        } catch (e: Exception) {
+            return Resource.Error("Error")
+        }
+        return Resource.Success(response)
+    }
+
+    suspend fun getCyrpto(id: String) : Resource<Crypto> {
+        val response = try {
+            api.getCyrpto(id)
+        } catch (e: Exception) {
+            return Resource.Error("Error")
+        }
+        return Resource.Success(response)
+    }
+}
