6476b5ad9b4b47f19ff6f134fac3c4e9aca47f14
Enis <enis.d.kaner@gmail.com>
2023-07-16 15:27:04 +0300

    AndroidEntryPoint added on MainActivity

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/view/MainActivity.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/view/MainActivity.kt
@@ -3,7 +3,9 @@ package com.eniskaner.eyojcryptoappwithkoin.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eniskaner.eyojcryptoappwithkoin.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
