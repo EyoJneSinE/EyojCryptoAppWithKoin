cc9a87749fe0d03c1b627e6d71d28b88f06d6ddd
Enis <enis.d.kaner@gmail.com>
2023-07-19 15:52:07 +0300

    CryptoRepository changed to interface

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/repo/CryptoRepository.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/repo/CryptoRepository.kt
@@ -2,30 +2,9 @@ package com.eniskaner.eyojcryptoappwithkoin.repo
 
 import com.eniskaner.eyojcryptoappwithkoin.model.Crypto
 import com.eniskaner.eyojcryptoappwithkoin.model.CryptoAllList
-import com.eniskaner.eyojcryptoappwithkoin.service.CryptoAPI
 import com.eniskaner.eyojcryptoappwithkoin.util.Resource
-import dagger.hilt.android.scopes.ActivityScoped
-import javax.inject.Inject
 
-@ActivityScoped
-class CryptoRepository @Inject constructor(
-    private val api: CryptoAPI
-) {
-    suspend fun getCyrptoList() : Resource<CryptoAllList> {
-        val response = try {
-            api.getCyrptoList()
-        } catch (e: Exception) {
-            return Resource.error("Error", null)
-        }
-        return Resource.success(response)
-    }
-
-    suspend fun getCyrpto(id: String) : Resource<Crypto> {
-        val response = try {
-            api.getCyrpto(id)
-        } catch (e: Exception) {
-            return Resource.error("Error", null)
-        }
-        return Resource.success(response)
-    }
+interface CryptoRepository {
+    suspend fun getCryptoList(): Resource<CryptoAllList>
+    suspend fun getCrypto(id: String): Resource<Crypto>
 }
