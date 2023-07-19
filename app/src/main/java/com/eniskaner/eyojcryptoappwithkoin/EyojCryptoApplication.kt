535f15a9658784c45df4dc908f95d4c32d166340
Enis <enis.d.kaner@gmail.com>
2023-07-19 15:57:17 +0300

    EyojCryptoApplication changed for koin

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/EyojCryptoApplication.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/EyojCryptoApplication.kt
@@ -1,8 +1,16 @@
 package com.eniskaner.eyojcryptoappwithkoin
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.eniskaner.eyojcryptoappwithkoin.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-@HiltAndroidApp
 class EyojCryptoApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@EyojCryptoApplication)
+            modules(appModule)
+        }
+    }
 }
