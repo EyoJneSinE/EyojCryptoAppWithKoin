1833808b1acb6600a302da7318a08ff9d25f70a2
Enis <enis.d.kaner@gmail.com>
2023-07-23 11:59:52 +0300

    CryptoAPI contents changed for Local database

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/network/service/CryptoAPI.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/network/service/CryptoAPI.kt
@@ -1,19 +1,18 @@
 package com.eniskaner.eyojcryptoappwithkoin.network.service
 
 import com.eniskaner.eyojcryptoappwithkoin.network.model.Crypto
-import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllList
-import retrofit2.Response
+import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllListItem
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CryptoAPI {
     //https://api.coinpaprika.com/v1/tickers
     @GET("tickers")
-    suspend fun getCyrptoList() : Response<CryptoAllList>
+    suspend fun getCyrptoList() : List<CryptoAllListItem>
 
     //https://api.coinpaprika.com/v1/coins/btc-bitcoin
     @GET("coins/{id}")
     suspend fun getCyrpto(
         @Path("id") id : String
-    ) : Response<Crypto>
+    ) : Crypto
 }
