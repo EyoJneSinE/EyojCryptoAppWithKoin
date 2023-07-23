1c088c34cf7bee30c541b325a0ea4a2f5016fd6c
Enis <enis.d.kaner@gmail.com>
2023-07-23 12:02:25 +0300

    CryptoRepositoryImplementattion contents changed for Local Database

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/network/repo/CryptoRepositoryImplementation.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/network/repo/CryptoRepositoryImplementation.kt
@@ -1,45 +1,32 @@
 package com.eniskaner.eyojcryptoappwithkoin.network.repo
 
+import com.eniskaner.eyojcryptoappwithkoin.data.local.CryptoDao
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCrypto
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCryptoList
 import com.eniskaner.eyojcryptoappwithkoin.network.model.Crypto
-import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllList
+import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllListItem
 import com.eniskaner.eyojcryptoappwithkoin.network.service.CryptoAPI
-import com.eniskaner.eyojcryptoappwithkoin.utils.Resource
 
-class CryptoRepositoryImplementation(private val api: CryptoAPI) : CryptoRepository {
-
-    override suspend fun getCryptoList(): Resource<CryptoAllList> {
-        return try {
-            val response = api.getCyrptoList()
-            if (response.isSuccessful) {
-                val data = response.body()
-                if (data != null) {
-                    Resource.success(data)
-                } else {
-                    Resource.error("Error", null)
-                }
-            } else {
-                Resource.error("Error", null)
-            }
-        } catch (e: Exception) {
-            Resource.error("Error", null)
-        }
+class CryptoRepositoryImplementation(
+    private val api: CryptoAPI,
+    private val localDataSource: CryptoDao
+) : CryptoRepository {
+    override suspend fun getCryptoList(): List<CryptoAllListItem> {
+        return api.getCyrptoList()
     }
-
-    override suspend fun getCrypto(id: String): Resource<Crypto> {
-        return try {
-            val response = api.getCyrpto(id)
-            if (response.isSuccessful) {
-                val data = response.body()
-                if (data != null) {
-                    Resource.success(data)
-                } else {
-                    Resource.error("Error", null)
-                }
-            } else {
-                Resource.error("Error", null)
-            }
-        } catch (e: Exception) {
-            Resource.error("Error", null)
-        }
+    override suspend fun getCrypto(id: String): Crypto  {
+        return api.getCyrpto(id)
+    }
+    override suspend fun saveCryptoList(localCryptoList: LocalCryptoList) {
+        return localDataSource.upsert(localCryptoList)
+    }
+    override fun getSavedCryptoList(): List<LocalCryptoList> {
+        return localDataSource.getLocalCryptoList()
+    }
+    override suspend fun savedCrypto(localCrypto: LocalCrypto) {
+        return localDataSource.upCrypto(localCrypto)
+    }
+    override fun getSavedCrypto(cryptoId: String): LocalCrypto {
+        return localDataSource.getLocalCrypto(cryptoId)
     }
 }
