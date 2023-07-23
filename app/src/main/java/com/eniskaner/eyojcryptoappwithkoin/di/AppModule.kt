53121f62f5081c92831c6f8d458fe63e9af0fcfb
Enis <enis.d.kaner@gmail.com>
2023-07-23 13:12:08 +0300

    use cases added on appModule

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/di/AppModule.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.eniskaner.eyojcryptoappwithkoin.di
 
+import com.eniskaner.eyojcryptoappwithkoin.data.local.CryptoDao
+import com.eniskaner.eyojcryptoappwithkoin.data.local.CryptoDatabase
+import com.eniskaner.eyojcryptoappwithkoin.data.usecase.CryptoDetailUseCase
+import com.eniskaner.eyojcryptoappwithkoin.data.usecase.CryptoListUseCase
 import com.eniskaner.eyojcryptoappwithkoin.network.repo.CryptoRepository
 import com.eniskaner.eyojcryptoappwithkoin.network.repo.CryptoRepositoryImplementation
 import com.eniskaner.eyojcryptoappwithkoin.network.service.CryptoAPI
 import com.eniskaner.eyojcryptoappwithkoin.utils.Constants.BASE_URL
 import com.eniskaner.eyojcryptoappwithkoin.ui.viewmodel.CryptoDetailViewModel
 import com.eniskaner.eyojcryptoappwithkoin.ui.viewmodel.CryptoListViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -21,14 +26,20 @@ val appModule = module {
             .create(CryptoAPI::class.java)
     }
 
+    single<CryptoDao> {
+        CryptoDatabase.getDatabase(androidContext()).getCryptoDao()
+    }
+
     single<CryptoRepository> {
         //since we defined retrofit above, this repository will asks for retrofit and we can simply
         //say get() in order to inject it even here
-        CryptoRepositoryImplementation(get())
+        CryptoRepositoryImplementation(get(),get())
 
         //since we are injecting the abstraction, we should explicitly state the
         //implementation and abstraction here
     }
+    single { CryptoListUseCase(get(), get()) }
+    single { CryptoDetailUseCase(get(), get()) }
 
     viewModel {
         //since we defined repo above, we can call get() here as well
