d74e108c7b172eba00fcc9082c560221b87a6b71
Enis <enis.d.kaner@gmail.com>
2023-07-23 12:45:01 +0300

    CryptoListViewModel contents changed for use case

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/viewmodel/CryptoListViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/viewmodel/CryptoListViewModel.kt
@@ -1,95 +1,95 @@
 package com.eniskaner.eyojcryptoappwithkoin.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCryptoList
+import com.eniskaner.eyojcryptoappwithkoin.data.usecase.CryptoListUseCase
 import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllListItem
-import com.eniskaner.eyojcryptoappwithkoin.network.repo.CryptoRepository
 import com.eniskaner.eyojcryptoappwithkoin.utils.Resource
-import com.eniskaner.eyojcryptoappwithkoin.utils.Status
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
-class CryptoListViewModel (
-    private val repository: CryptoRepository
-) : ViewModel(), CoroutineScope {
-    private val _cryptoList = MutableLiveData<Resource<List<CryptoAllListItem>>>()
-    val cryptoList: LiveData<Resource<List<CryptoAllListItem>>> get() = _cryptoList
-    private val _errorMessage = MutableLiveData<String>()
-    val errorMessage: LiveData<String> get() = _errorMessage
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> get() = _isLoading
-    private var initialCryptoList = listOf<CryptoAllListItem>()
+class CryptoListViewModel(private val cryptoListUseCase: CryptoListUseCase) : ViewModel() {
+    private val _cryptoList = MutableStateFlow<Resource<List<CryptoAllListItem>>>(Resource.Loading())
+    val cryptoList: StateFlow<Resource<List<CryptoAllListItem>>> = _cryptoList
+
+    private val _errorMessage = MutableStateFlow("")
+    val errorMessage: StateFlow<String> = _errorMessage
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private var initialCryptoList: List<CryptoAllListItem>? = null
     private var isSearchStarting = true
-    private val job = Job()
 
-    private val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-        println("Error: ${throwable.localizedMessage}")
-        _errorMessage.postValue(throwable.localizedMessage ?: "Error!")
-        _isLoading.postValue(false)
-    }
-    override val coroutineContext: CoroutineContext get() = Dispatchers.IO + job + exceptionHandler
     init {
         loadCryptos()
     }
     fun searchCryptoList(query: String) {
         val listToSearch = if (isSearchStarting) {
-            _cryptoList.value?.data ?: emptyList()
+            _cryptoList.value.data ?: emptyList()
         } else {
-            initialCryptoList
+            initialCryptoList ?: emptyList()
         }
-        launch(Dispatchers.Default) {
-            if (query.isEmpty()) {
-                _cryptoList.postValue(Resource.success(initialCryptoList))
-                isSearchStarting = true
-                return@launch
-            }
-            val results = listToSearch.filter {
-                it.symbol.contains(query.trim(), ignoreCase = true)
-            }
-            if (isSearchStarting) {
-                initialCryptoList = _cryptoList.value?.data ?: emptyList()
-                isSearchStarting = false
-            }
-            _cryptoList.postValue(Resource.success(results))
+        if (query.isEmpty()) {
+            _cryptoList.value = Resource.Success(initialCryptoList ?: emptyList())
+            isSearchStarting = true
+            return
         }
+        val results = listToSearch.filter {
+            it.id.contains(query.trim(), ignoreCase = true)
+        }
+        if (isSearchStarting) {
+            initialCryptoList = _cryptoList.value.data
+            isSearchStarting = false
+        }
+        _cryptoList.value = Resource.Success(results)
     }
-    fun loadCryptos() {
-        _isLoading.postValue(true)
-        _errorMessage.postValue("")
-        launch {
-            val result = repository.getCryptoList()
-            when (result.status) {
-                Status.SUCCESS -> {
-                    val cryptoItems = result.data!!.mapIndexed { index, item ->
-                        CryptoAllListItem(
-                            item.beta_value, item.circulating_supply, item.first_data_at, item.id,
-                            item.last_updated, item.max_supply, item.name, item.quotes, item.rank, item.symbol, item.total_supply
-                        )
+    private fun loadCryptos() {
+        _isLoading.value = true
+        _errorMessage.value = ""
+        viewModelScope.launch {
+            try {
+                cryptoListUseCase.getCryptoList().collect { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            val cryptoItems = result.data?.mapIndexed { index, item ->
+                                CryptoAllListItem(
+                                    item.beta_value, item.circulating_supply, item.first_data_at, item.id,
+                                    item.last_updated, item.max_supply, item.name, item.quotes, item.rank, item.symbol, item.total_supply
+                                )
+                            } ?: emptyList()
+                            initialCryptoList = cryptoItems
+                            _cryptoList.value = Resource.Success(cryptoItems)
+                        }
+                        is Resource.Error -> {
+                            _errorMessage.value = result.message ?: "Error occurred!"
+                            _cryptoList.value = Resource.Error(result.message ?: "Error occurred!", null)
+                        }
+                        is Resource.Loading -> {
+                            _cryptoList.value = Resource.Loading()
+                        }
                     }
-                    _cryptoList.postValue(Resource.success(cryptoItems))
                 }
-                Status.ERROR -> {
-                    _errorMessage.postValue(result.message ?: "Error occurred!")
-                    _cryptoList.postValue(Resource.error(result.message ?: "Error occurred!", null))
-                }
-                Status.LOADING -> {
-                    _cryptoList.postValue(Resource.loading(null))
-                }
+            } catch (e: Exception) {
+                _errorMessage.value = "Error occurred!"
+                _cryptoList.value = Resource.Error("Error occurred!", null)
             }
-            _isLoading.postValue(false)
+            _isLoading.value = false
         }
     }
     fun refreshCryptoList() {
-        val currentCryptoList = _cryptoList.value?.data
-        _cryptoList.postValue(Resource.success(currentCryptoList))
+        val currentCryptoList = _cryptoList.value.data
+        _cryptoList.value = Resource.Success(currentCryptoList ?: emptyList())
     }
-    override fun onCleared() {
-        super.onCleared()
-        job.cancel()
+    fun saveCryptoListToDB(localCryptoList: LocalCryptoList) {
+        viewModelScope.launch {
+            cryptoListUseCase.saveCryptoList(localCryptoList)
+        }
+    }
+    fun getSavedCryptoListFromDB(): Flow<Resource<List<CryptoAllListItem>>> {
+        return cryptoListUseCase.getSavedCryptoList()
     }
 }
