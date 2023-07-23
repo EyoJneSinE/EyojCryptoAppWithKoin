8cb6bbb558214948c01f494a4a21e16b1868996f
Enis <enis.d.kaner@gmail.com>
2023-07-23 12:19:41 +0300

    adapter contents changed and diffutil added

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/adapter/CryptoListAdapter.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/adapter/CryptoListAdapter.kt
@@ -2,15 +2,15 @@ package com.eniskaner.eyojcryptoappwithkoin.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.eniskaner.eyojcryptoappwithkoin.databinding.RecyclerRowBinding
 import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllListItem
 
 class CryptoListAdapter(
-    private val cryptoList: List<CryptoAllListItem>,
     private val onItemClick: (CryptoAllListItem) -> Unit
-) : RecyclerView.Adapter<CryptoListAdapter.CryptoViewHolder>() {
-
+) : ListAdapter<CryptoAllListItem, CryptoListAdapter.CryptoViewHolder>(CryptoListDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CryptoViewHolder {
         val binding = RecyclerRowBinding.inflate(
             LayoutInflater.from(parent.context),
@@ -19,33 +19,35 @@ class CryptoListAdapter(
         )
         return CryptoViewHolder(binding)
     }
-
     override fun onBindViewHolder(holder: CryptoViewHolder, position: Int) {
-        val crypto = cryptoList[position]
+        val crypto = getItem(position)
         holder.bind(crypto)
     }
-
-    override fun getItemCount(): Int = cryptoList.size
-
     inner class CryptoViewHolder(
         private val binding: RecyclerRowBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-
         init {
             binding.root.setOnClickListener {
-                val position = adapterPosition
+                val position = bindingAdapterPosition
                 if (position != RecyclerView.NO_POSITION) {
-                    val crypto = cryptoList[position]
+                    val crypto = getItem(position)
                     onItemClick(crypto)
                 }
             }
         }
-
         fun bind(crypto: CryptoAllListItem) {
             binding.apply {
                 cryptoNameText.text = crypto.symbol
                 cryptoPriceText.text = crypto.quotes.USD.price.toString()
             }
+        }
+    }
+    class CryptoListDiffCallback : DiffUtil.ItemCallback<CryptoAllListItem>() {
+        override fun areItemsTheSame(oldItem: CryptoAllListItem, newItem: CryptoAllListItem): Boolean {
+            return oldItem.quotes.USD.price == newItem.quotes.USD.price
+        }
+        override fun areContentsTheSame(oldItem: CryptoAllListItem, newItem: CryptoAllListItem): Boolean {
+            return oldItem == newItem
         }
     }
 }
