24096f041ce5e59e47a664dbdaa3a1937269f33b
Enis <enis.d.kaner@gmail.com>
2023-07-23 12:01:06 +0300

    CryptoRepository contents changed for Local Database

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/network/repo/CryptoRepository.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/network/repo/CryptoRepository.kt
@@ -1,10 +1,19 @@
 package com.eniskaner.eyojcryptoappwithkoin.network.repo
 
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCrypto
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCryptoList
 import com.eniskaner.eyojcryptoappwithkoin.network.model.Crypto
-import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllList
-import com.eniskaner.eyojcryptoappwithkoin.utils.Resource
+import com.eniskaner.eyojcryptoappwithkoin.network.model.CryptoAllListItem
 
 interface CryptoRepository {
-    suspend fun getCryptoList(): Resource<CryptoAllList>
-    suspend fun getCrypto(id: String): Resource<Crypto>
+    suspend fun getCryptoList(): List<CryptoAllListItem>
+    suspend fun getCrypto(id: String): Crypto
+
+    suspend fun saveCryptoList(localCryptoList: LocalCryptoList)
+
+    fun getSavedCryptoList(): List<LocalCryptoList>
+
+    suspend fun savedCrypto(localCrypto: LocalCrypto)
+
+    fun getSavedCrypto(cryptoId: String): LocalCrypto
 }
