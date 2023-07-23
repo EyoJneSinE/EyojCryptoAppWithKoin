55e3fce950d56df7d233023689cd64de5e7848e6
Enis <enis.d.kaner@gmail.com>
2023-07-23 12:57:44 +0300

    CryptoDetailViewModel contents changed for use case

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/viewmodel/CryptoDetailViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/viewmodel/CryptoDetailViewModel.kt
@@ -1,12 +1,19 @@
 package com.eniskaner.eyojcryptoappwithkoin.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCrypto
+import com.eniskaner.eyojcryptoappwithkoin.data.usecase.CryptoDetailUseCase
 import com.eniskaner.eyojcryptoappwithkoin.network.model.Crypto
-import com.eniskaner.eyojcryptoappwithkoin.network.repo.CryptoRepository
 import com.eniskaner.eyojcryptoappwithkoin.utils.Resource
+import kotlinx.coroutines.flow.Flow
 
-class CryptoDetailViewModel(private val repository: CryptoRepository): ViewModel() {
-    suspend fun getCrypto(id: String) : Resource<Crypto> {
-        return repository.getCrypto(id)
+class CryptoDetailViewModel(private val detailUseCase: CryptoDetailUseCase): ViewModel() {
+
+    suspend fun getCrypto(id: String) : Flow<Resource<Crypto>> {
+        return detailUseCase.getCrypto(id)
+    }
+
+    fun getLocalCrypto(cryptoId: String): Flow<List<LocalCrypto>>  {
+        return detailUseCase.getSavedCrypto(cryptoId)
     }
 }
