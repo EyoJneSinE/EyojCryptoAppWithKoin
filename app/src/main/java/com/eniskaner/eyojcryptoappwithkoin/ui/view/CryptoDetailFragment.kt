3646761c916db1d8968ee1e246b4fd5d111ba1b1
Enis <enis.d.kaner@gmail.com>
2023-07-23 13:07:44 +0300

    CryptoDetailFragment contents changed with viewmodel

--- app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/view/CryptoDetailFragment.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoappwithkoin/ui/view/CryptoDetailFragment.kt
@@ -2,65 +2,97 @@ package com.eniskaner.eyojcryptoappwithkoin.ui.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
 import com.eniskaner.eyojcryptoappwithkoin.R
 import com.eniskaner.eyojcryptoappwithkoin.base.BaseFragment
+import com.eniskaner.eyojcryptoappwithkoin.data.model.LocalCrypto
 import com.eniskaner.eyojcryptoappwithkoin.databinding.FragmentCryptoDetailBinding
-import com.eniskaner.eyojcryptoappwithkoin.network.model.Crypto
-import com.eniskaner.eyojcryptoappwithkoin.utils.Status
+import com.eniskaner.eyojcryptoappwithkoin.network.model.toLocalCrypto
 import com.eniskaner.eyojcryptoappwithkoin.ui.viewmodel.CryptoDetailViewModel
+import com.eniskaner.eyojcryptoappwithkoin.utils.Resource
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CryptoDetailFragment : BaseFragment<FragmentCryptoDetailBinding>(), ViewModelStoreOwner {
     private val viewModel: CryptoDetailViewModel by viewModel()
-    private var selectedCrypto: Crypto? = null
-    override fun setBinding(): FragmentCryptoDetailBinding = FragmentCryptoDetailBinding.inflate(layoutInflater)
+    private var selectedCrypto: LocalCrypto? = null
+    override fun setBinding(): FragmentCryptoDetailBinding =
+        FragmentCryptoDetailBinding.inflate(layoutInflater)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val cryptoId = arguments?.getString("cryptoId")
         val price = arguments?.getDouble("price")
-        fetchCryptoData(cryptoId, price)
-        binding.cryptoDescription.setOnClickListener {
-            binding.cryptoDescription.maxLines = Int.MAX_VALUE
-            binding.cryptoDescription.ellipsize = null
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                cryptoId?.let { id ->
+                    viewModel.getCrypto(id).collect { crypto ->
+                        val localCrypto = crypto.data?.toLocalCrypto()
+                        if (localCrypto != null) {
+                            updateCryptoDetail(localCrypto, price)
+                        } else {
+                            fetchCryptoData(cryptoId, price)
+                        }
+                    }
+                }
+            }
+        }
+        binding.cryptoDescription.apply {
+            setOnClickListener {
+                maxLines = Int.MAX_VALUE
+                ellipsize = null
+            }
         }
     }
     private fun fetchCryptoData(cryptoId: String?, price: Double?) {
-        lifecycleScope.launch {
-            cryptoId?.let { id ->
-                val resource = viewModel.getCrypto(id)
-                when (resource.status) {
-                    Status.SUCCESS -> {
-                        selectedCrypto = resource.data
-                        updateCryptoDetail(price)
-                    }
-                    Status.ERROR -> {
-                        val errorMessage = resource.message ?: "Error!"
-                        // Handle error
-                    }
-                    Status.LOADING -> {
-                        val loading = resource.message ?: "Loading!"
-                        // Show loading state
+        viewLifecycleOwner.lifecycleScope.apply {
+            launch {
+                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    cryptoId?.let { id ->
+                        val resource = viewModel.getCrypto(id)
+                        resource.collect { result ->
+                            when (result) {
+                                is Resource.Success -> {
+                                    val selectedCrypto = result.data?.toLocalCrypto()
+                                    updateCryptoDetail(selectedCrypto, price)
+                                }
+                                is Resource.Error -> {
+                                    binding.apply {
+                                        loadingProgressBar.visibility = View.GONE
+                                        errorTextView.visibility = View.VISIBLE
+                                        errorTextView.text = result.message ?: "Error!"
+                                    }
+                                }
+                                is Resource.Loading -> {
+                                    binding.apply {
+                                        errorTextView.visibility = View.GONE
+                                        loadingProgressBar.visibility = View.VISIBLE
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-    private fun updateCryptoDetail(price: Double?) {
+    private fun updateCryptoDetail(selectedCrypto: LocalCrypto?, price: Double?) {
         selectedCrypto?.let { crypto ->
-            binding.cryptoName.text = crypto.name
-            binding.cryptoPrice.text = price?.toString()
-            binding.cryptoDescription.text = crypto.description
+            binding.apply {
+                cryptoName.text = crypto.name
+                cryptoPrice.text = price.toString()
+                cryptoDescription.text = crypto.description
 
-            Glide.with(requireContext())
-                .load(crypto.logo)
-                .placeholder(R.drawable.ic_launcher_foreground)
-                .error(R.drawable.ic_error)
-                .centerCrop()
-                .into(binding.imageLogo)
+                Glide.with(requireContext())
+                    .load(crypto.logo)
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .error(R.drawable.ic_error)
+                    .centerCrop()
+                    .into(binding.imageLogo)
+            }
         }
     }
     fun refreshData() {
